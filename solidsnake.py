534da6e5fe079611d9d962f1480f0e4836b71533
tkrger <tokargergo2005@gmail.com>
2025-11-02 17:47:02 +0100

    v0.3

--- solidsnake.py
+++ solidsnake.py
@@ -11,6 +11,22 @@ pygame.display.set_caption("Solid Snake")
 icon = pygame.image.load("solidsnake.png")
 pygame.display.set_icon(icon)
 
+def inditas_kiiratas(szokoz):
+    inditas = betutipus.render("Nyomd meg a SPACE-t a kezdéshez.", True, (255, 255, 255))
+
+    if szokoz == True:
+        screen.blit(inditas, (1000,1000)) 
+    else:
+        screen.blit(inditas, (180,100))
+
+
+betutipus = pygame.font.SysFont(None, 35)
+
+def pontszam_kiir(pont):
+    szoveg = betutipus.render("Pontszám: " + str(pont), True, (255, 255, 255))
+    screen.blit(szoveg, (10, 10))
+
+pontszam = 0
 
 kigyo_meret = 10
 kigyo_sebesseg = 15
@@ -21,10 +37,10 @@ x_valt, y_valt = 0, 0
 kigyo_lista = []
 kigyo_hossza = 1
 
-
 etel_x = round(random.randrange(0, szelesseg - kigyo_meret) / 10.0) * 10.0
 etel_y = round(random.randrange(0, magassag - kigyo_meret) / 10.0) * 10.0
 
+szokoz_lenyomva = False
 
 ora = pygame.time.Clock()
 running = True
@@ -34,8 +50,10 @@ while running:
             running = False
 
         if esemeny.type == pygame.KEYDOWN:
-           
-            if esemeny.type == pygame.KEYDOWN:
+            if esemeny.key == pygame.K_SPACE:
+                szokoz_lenyomva = True
+
+            if esemeny.type == pygame.KEYDOWN and szokoz_lenyomva == True:
                 if esemeny.key == pygame.K_LEFT:
                     x_valt = -kigyo_meret
                     y_valt = 0
@@ -72,6 +90,7 @@ while running:
         etel_x = round(random.randrange(0, szelesseg - kigyo_meret) / 10.0) * 10.0
         etel_y = round(random.randrange(0, magassag - kigyo_meret) / 10.0) * 10.0
         kigyo_hossza += 1
+        pontszam += 1
     
     if x_kigyo < 0 or x_kigyo >= szelesseg or y_kigyo < 0 or y_kigyo >= magassag:
         running = False
@@ -81,6 +100,8 @@ while running:
             running = False
 
 
+    inditas_kiiratas(szokoz_lenyomva)
+    pontszam_kiir(pontszam)
     pygame.display.update()
     ora.tick(kigyo_sebesseg)
 
